05035df3c45149abb64c0cf2fbe6192d1d85de03
Xttb4 <Pavel.92.nsk@gmail.com>
2021-08-02 16:18:19 +0700

    v1

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+    let tipPercentages = [0, 5, 10, 15, 20]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSellection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSellection
+        let grandTotal = orderAmount + tipValue
+        return grandTotal / peopleCount
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Сумма", text: $checkAmount)
+                    Picker("Количество человек", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) человек(а)")
+                        }
+                    }
+                }
+                Section (header: Text("Сколько чаевых вы хотите оставить?")) {
+                    Picker("Процент чаевых", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    } .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                }
+            } .navigationBarTitle("Калькулятор чаевых", displayMode: .inline)
+        }
     }
 }
 
